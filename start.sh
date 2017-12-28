c4758ce4261b0efff783709a88e7a906379086fd
Joway Wang <joway.w@gmail.com>
2017-12-28 17:48:19 +0800

    add version custom

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 CONFIG_FILE=/etc/burrow/burrow.toml
+KAFKA_VERSION=${KAFKA_VERSION:-0.10.1.0}
 
 sed -i "s/{ZOOKEEPER_SERVERS}/${ZOOKEEPER_SERVERS}/" $CONFIG_FILE
 sed -i "s/{KAFKA_BROKERS}/${KAFKA_BROKERS}/" $CONFIG_FILE
+sed -i "s/{KAFKA_VERSION}/${KAFKA_VERSION}/" $CONFIG_FILE
 
 echo "start"
 cat $CONFIG_FILE
