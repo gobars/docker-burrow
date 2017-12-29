9df43e4cb69645b6934c0114d303ae00e0d3f6a6
Joway Wang <joway.w@gmail.com>
2017-12-29 11:55:15 +0800

    add influxdb support

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 CONFIG_FILE=/etc/burrow/burrow.toml
+INFLUXDB_TMPL_FILE=/etc/burrow/tmpl/http-influxdb.tmpl
 KAFKA_VERSION=${KAFKA_VERSION:-0.10.1.0}
 
 sed -i "s/{ZOOKEEPER_SERVERS}/${ZOOKEEPER_SERVERS}/" $CONFIG_FILE
 sed -i "s/{KAFKA_BROKERS}/${KAFKA_BROKERS}/" $CONFIG_FILE
 sed -i "s/{KAFKA_VERSION}/${KAFKA_VERSION}/" $CONFIG_FILE
+
+sed -i "s~{INFLUXDB_URL}~${INFLUXDB_URL}~" $CONFIG_FILE
 
 echo "start"
 cat $CONFIG_FILE
